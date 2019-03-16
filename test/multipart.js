9860eb742adb85a7c77e3b1c6309338429a0af99
Douglas Christopher Wilson <doug@somethingdoug.com>
2019-03-16 00:17:42 -0400

    build: eslint@4.19.1

--- test/multipart.js
+++ test/multipart.js
@@ -10,111 +10,111 @@ var should = require('should');
 describe('multipart()', function(){
   it('should ignore GET', function(done){
     request(createServer())
-    .get('/body')
-    .field('user', 'Tobi')
-    .expect(200, {}, done)
+      .get('/body')
+      .field('user', 'Tobi')
+      .expect(200, {}, done)
   })
 
   describe('with multipart/form-data', function(){
     it('should populate req.body', function(done){
       request(createServer())
-      .post('/body')
-      .field('user', 'Tobi')
-      .expect(200, { user: 'Tobi' }, done)
+        .post('/body')
+        .field('user', 'Tobi')
+        .expect(200, { user: 'Tobi' }, done)
     })
 
     it('should handle duplicated middleware', function (done) {
       var app = connect()
-      .use(multipart())
-      .use(multipart())
-      .use(function (req, res) {
-        res.setHeader('Content-Type', 'application/json; charset=utf-8')
-        res.end(JSON.stringify(req.body))
-      })
+        .use(multipart())
+        .use(multipart())
+        .use(function (req, res) {
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          res.end(JSON.stringify(req.body))
+        })
 
       request(app)
-      .post('/body')
-      .field('user', 'Tobi')
-      .expect(200, { user: 'Tobi' }, done)
+        .post('/body')
+        .field('user', 'Tobi')
+        .expect(200, { user: 'Tobi' }, done)
     })
 
     it('should support files', function(done){
       request(createServer())
-      .post('/files')
-      .attach('text', Buffer.from('some text here'), 'foo.txt')
-      .expect(200)
-      .expect(shouldDeepIncludeInBody({
-        text: {
-          name: 'foo.txt',
-          originalFilename: 'foo.txt',
-          size: 14,
-          type: 'text/plain'
-        }
-      }))
-      .end(done)
+        .post('/files')
+        .attach('text', Buffer.from('some text here'), 'foo.txt')
+        .expect(200)
+        .expect(shouldDeepIncludeInBody({
+          text: {
+            name: 'foo.txt',
+            originalFilename: 'foo.txt',
+            size: 14,
+            type: 'text/plain'
+          }
+        }))
+        .end(done)
     })
 
     it('should work with multiple fields', function(done){
       request(createServer())
-      .post('/body')
-      .field('user', 'Tobi')
-      .field('age', '1')
-      .expect(200, { user: 'Tobi', age: '1' }, done)
+        .post('/body')
+        .field('user', 'Tobi')
+        .field('age', '1')
+        .expect(200, { user: 'Tobi', age: '1' }, done)
     })
 
     it('should handle duplicated fields', function (done) {
       request(createServer())
-      .post('/body')
-      .field('user', 'Tobi')
-      .field('user', 'Loki')
-      .field('user', 'Poki')
-      .expect(200, { user: [ 'Tobi', 'Loki', 'Poki' ] }, done)
+        .post('/body')
+        .field('user', 'Tobi')
+        .field('user', 'Loki')
+        .field('user', 'Poki')
+        .expect(200, { user: [ 'Tobi', 'Loki', 'Poki' ] }, done)
     })
 
     it('should support nesting', function(done){
       request(createServer())
-      .post('/body')
-      .field('user[name][first]', 'tobi')
-      .field('user[name][last]', 'holowaychuk')
-      .field('user[age]', '1')
-      .field('species', 'ferret')
-      .expect(200, {
-        species: 'ferret',
-        user: {
-          age: '1',
-          name: { first: 'tobi', last: 'holowaychuk' }
-        }
-      }, done)
+        .post('/body')
+        .field('user[name][first]', 'tobi')
+        .field('user[name][last]', 'holowaychuk')
+        .field('user[age]', '1')
+        .field('species', 'ferret')
+        .expect(200, {
+          species: 'ferret',
+          user: {
+            age: '1',
+            name: { first: 'tobi', last: 'holowaychuk' }
+          }
+        }, done)
     })
 
     it('should support multiple files of the same name', function(done){
       request(createServer())
-      .post('/files')
-      .attach('text', Buffer.from('some text here'), 'foo.txt')
-      .attach('text', Buffer.from('some more text stuff'), 'bar.txt')
-      .expect(200)
-      .expect(shouldDeepIncludeInBody({
-        text: [
-          { name: 'foo.txt' },
-          { name: 'bar.txt' }
-        ]
-      }))
-      .end(done)
+        .post('/files')
+        .attach('text', Buffer.from('some text here'), 'foo.txt')
+        .attach('text', Buffer.from('some more text stuff'), 'bar.txt')
+        .expect(200)
+        .expect(shouldDeepIncludeInBody({
+          text: [
+            { name: 'foo.txt' },
+            { name: 'bar.txt' }
+          ]
+        }))
+        .end(done)
     })
 
     it('should support nested files', function(done){
       request(createServer())
-      .post('/files')
-      .attach('docs[foo]', Buffer.from('some text here'), 'foo.txt')
-      .attach('docs[bar]', Buffer.from('some more text stuff'), 'bar.txt')
-      .expect(200)
-      .expect(shouldDeepIncludeInBody({
-        docs: {
-          foo: { name: 'foo.txt' },
-          bar: { name: 'bar.txt' }
-        }
-      }))
-      .end(done)
+        .post('/files')
+        .attach('docs[foo]', Buffer.from('some text here'), 'foo.txt')
+        .attach('docs[bar]', Buffer.from('some more text stuff'), 'bar.txt')
+        .expect(200)
+        .expect(shouldDeepIncludeInBody({
+          docs: {
+            foo: { name: 'foo.txt' },
+            bar: { name: 'bar.txt' }
+          }
+        }))
+        .end(done)
     })
 
     it('should next(err) on multipart failure', function(done){
@@ -155,18 +155,18 @@ describe('multipart()', function(){
 
     it('should default req.files to {}', function(done){
       request(createServer())
-      .post('/body')
-      .expect(200, {}, done)
+        .post('/body')
+        .expect(200, {}, done)
     })
 
     it('should return 413 on maxFilesSize exceeded', function (done) {
       var max = Math.pow(2, 9)
 
       request(createServer({ maxFilesSize: max }))
-      .post('/files')
-      .field('user[name]', 'Tobi')
-      .attach('text', Buffer.alloc(max + 1, 'x'), 'foo.txt')
-      .expect(413, done)
+        .post('/files')
+        .field('user[name]', 'Tobi')
+        .attach('text', Buffer.alloc(max + 1, 'x'), 'foo.txt')
+        .expect(413, done)
     })
   })
 })
